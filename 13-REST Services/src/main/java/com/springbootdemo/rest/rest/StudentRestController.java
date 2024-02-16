b5c716382b8815c3c3b40e52ec5ef726ba72c206
Lakshan Chamoditha Perera <perera.alc2000@gmail.com>
2024-02-16 18:01:03 +0530

    Generic Exception handling in Rest Controller

--- 13-REST Services/src/main/java/com/springbootdemo/rest/rest/StudentRestController.java
+++ 13-REST Services/src/main/java/com/springbootdemo/rest/rest/StudentRestController.java
@@ -40,4 +40,14 @@ public class StudentRestController {
         return new ResponseEntity<StudentErrorResponse>(error, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+        StudentErrorResponse error = new StudentErrorResponse();
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+        error.setMessage(exc.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<StudentErrorResponse>(error, HttpStatus.NOT_FOUND);
+    }
+
+
 }
