690a234f160ff9f526b2506d0665be38e48c6e3f
Lakshan Chamoditha Perera <perera.alc2000@gmail.com>
2024-02-16 12:26:31 +0530

    Save object using Entitymanager & DAO

--- 12-JPA CRUD Demo/src/main/java/com/springbootdemo/crudapp/CrudappApplication.java
+++ 12-JPA CRUD Demo/src/main/java/com/springbootdemo/crudapp/CrudappApplication.java
@@ -1,5 +1,7 @@
 package com.springbootdemo.crudapp;
 
+import com.springbootdemo.crudapp.dao.StudentDAO;
+import com.springbootdemo.crudapp.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,14 +10,26 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class CrudappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrudappApplication.class, args);
-	}
-	@Bean
-	public CommandLineRunner commandLineRunner() {
-		return args -> {
-			System.out.println("Hello from command line runner");
-		};
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CrudappApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
+        return args -> {
+            createStudent(studentDAO);
+        };
+    }
+
+    private void createStudent(StudentDAO studentDAO) {
+        System.out.println("Creating student");
+        Student student = new Student("John", "Doe", "johndoe@email.com");
+
+        System.out.println("Saving student");
+        studentDAO.save(student);
+
+        System.out.println("Student saved. Generated id: " + student.getId());
+
+    }
 
 }
