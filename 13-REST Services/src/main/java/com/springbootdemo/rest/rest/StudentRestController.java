c0641ee62dd6fc3141795bce74975008a7f35f0c
Lakshan Chamoditha Perera <perera.alc2000@gmail.com>
2024-02-16 17:35:01 +0530

    @PathVariable annotation

--- 13-REST Services/src/main/java/com/springbootdemo/rest/rest/StudentRestController.java
+++ 13-REST Services/src/main/java/com/springbootdemo/rest/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.springbootdemo.rest.rest;
 
 import com.springbootdemo.rest.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +12,25 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData() {
+        students = List.of(
+                new Student("John", "Doe"),
+                new Student("Jane", "Doe"),
+                new Student("Mary", "Doe")
+        );
+    }
+
     @GetMapping("/students")
     public List<Student> getStudents() {
-        return List.of(new Student("John", "Doe"), new Student("Jane", "Doe"), new Student("Tom", "Doe"), new Student("Jerry", "Doe"), new Student("Jerry", "Doe"), new Student("Jerry", "Doe"));
+        return students;
+    }
+
+    @GetMapping("/students/{id}")
+    public Student getStudent(@PathVariable int id) {
+        return new Student("John", "Doe");
     }
 
 }
