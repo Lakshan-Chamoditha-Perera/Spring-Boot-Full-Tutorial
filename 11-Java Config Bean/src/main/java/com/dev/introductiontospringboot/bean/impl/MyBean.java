cee843d9fde9ee25ef4c470ddb833ef9563db993
Lakshan Chamoditha Perera <perera.alc2000@gmail.com>
2024-02-16 09:04:34 +0530

    Java Config Beans (naming beans)

--- 11-Java Config Bean/src/main/java/com/dev/introductiontospringboot/bean/impl/MyBean.java
+++ 11-Java Config Bean/src/main/java/com/dev/introductiontospringboot/bean/impl/MyBean.java
@@ -3,16 +3,16 @@ package com.dev.introductiontospringboot.bean.impl;
 import com.dev.introductiontospringboot.bean.SuperInterface;
 import org.springframework.stereotype.Component;
 
-@Component("myBean")
+//providing a custom name to the bean
+@Component("MY_BEAN")
 public class MyBean implements SuperInterface {
-
     public MyBean() {
         System.out.println("Constructor called");
     }
-
 
     @Override
     public void getDailyWorkout() {
         System.out.println("MyBean is running");
     }
 }
+
