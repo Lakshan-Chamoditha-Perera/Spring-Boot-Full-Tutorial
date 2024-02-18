d4026af05bd3d1466d29a761c61dafd99d0b0be5
Lakshan Chamoditha Perera <perera.alc2000@gmail.com>
2024-02-18 13:21:46 +0530

    Spring Boot Security Basics added

---
+++ 18-Spring Boot Security (Introduction)/src/main/java/com/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.springboot.cruddemo.dao;
+
+import com.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+@RepositoryRestResource(path = "members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    // no need to write any code
+}
+
+//url -> http://localhost:8080//magic-api/members
