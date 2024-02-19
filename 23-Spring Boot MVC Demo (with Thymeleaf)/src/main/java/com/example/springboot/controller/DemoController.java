294c7debac750c2e8d16206d88868cb55e877e67
Lakshan Chamoditha Perera <perera.alc2000@gmail.com>
2024-02-19 13:53:17 +0530

    Spring Boot MVC (with Form)

--- 23-Spring Boot MVC Demo (with Thymeleaf)/src/main/java/com/example/springboot/controller/DemoController.java
+++ 23-Spring Boot MVC Demo (with Thymeleaf)/src/main/java/com/example/springboot/controller/DemoController.java
@@ -3,6 +3,7 @@ package com.example.springboot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Date;
 
@@ -12,6 +13,11 @@ public class DemoController {
     @GetMapping
     public String get(Model model){
         model.addAttribute("theDate",new Date());
-        return "hello";
+        return "hello"; //html name
+    }
+
+    @RequestMapping("/showForm")
+    public String showForm(Model model){
+        return "my_form"; //html name
     }
 }
